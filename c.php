e23acb965aef99b05588d90275a7ad86c608a520
ネギシユウダイ <67693802+Negishi-Yudai@users.noreply.github.com>
2020-10-21 11:40:36 +0900

    Update c.php

--- c.php
+++ c.php
@@ -5,9 +5,9 @@ function mul($max) {
 };
 
 echo mul(10);
-?>
 
-<?php
+
+
 function f($a, $b){
     $sum = $a + $b;
     return $sum;
@@ -37,36 +37,36 @@ function max_array($arr){
    return $max_number;
 }
 echo max_array(array(7,1,3,4,5));
-?>
 
-<!--5-->
-<?php
-$str = "<h1>あいうえお</h1>"
-."<p>取り除く</p>";
-echo strip_tags($str)."<br>";
-?>
-<?php
+
+
+
+$str = "あいうえお"
+."取り除く";
+echo strip_tags($str)."";
+
+
 $stack = array("orange","banana");
 array_push($stack,"apple","raspberry");
 echo ($stack);
-?>
 
-<?php
+
+
 $array1 = [1,2,3];
 $array2 = [4,5,6];
 $array3 = [7,8,9];
 
 $array = array_merge($array1,$array2,$array3);
 echo ($array);
-?>
 
-<?php
+
+
 $time = mktime(0,0,0,1,1,2020);
 var_dump(date('Y年m月h時i分s秒',$time));
 echo ('<br/>');
-?>
 
-<?php
+
+
 date_default_timezone_set('Asia/Tokyo');
 echo '<p>',date('Y/m/d H:i:s'),'</p>';
 ?>
